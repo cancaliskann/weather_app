4b2c545876f60444125ff2907ff35a34f2c01dd2
cancaliskann <podcastwerv2017@gmail.com>
2024-02-29 20:28:40 +0200

    now it can change the values on the screen

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     const apiKey = "9c867a6081632b9e75d0200e0b7234ff";
-    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=hamburg";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-    async function checkWeather(){
-        const response = await fetch(apiUrl + `&appid=${apiKey}`);
-        var data = await response.json();
+    const searchBox = document.querySelector(".search input");
+    const searchBtn = document.querySelector(".search button");
+
+    async function checkWeather(city) {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        const data = await response.json();
 
         console.log(data);
         console.log(data.name);
+
         document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
     }
 
-    checkWeather();
+    searchBtn.addEventListener("click", () => {
+        checkWeather(searchBox.value);
+    });
 });
