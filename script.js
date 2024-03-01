f78b16898e2bbb3ecccae27f32b269e62996132d
cancaliskann <podcastwerv2017@gmail.com>
2024-03-01 02:11:36 +0200

    added diffrent weather scnerios added weather icons *mostly from google

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const searchBox = document.querySelector(".search input");
     const searchBtn = document.querySelector(".search button");
+    const weatherIcon = document.querySelector(".weather-icon")
 
     async function checkWeather(city) {
         const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -16,6 +17,22 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if(data.weather[0].main =="Clouds"){
+            weatherIcon.src = "images/cloudy.png";
+        }
+        else if(data.weather[0].main =="Clear"){
+            weatherIcon.src = "images/sunny.png";
+        }
+        else if(data.weather[0].main =="Rain"){
+            weatherIcon.src = "images/rain.png";
+        }
+        else if(data.weather[0].main =="Drizzle"){
+            weatherIcon.src = "images/drizzle.png";
+        }
+        else if(data.weather[0].main =="Mist"){
+            weatherIcon.src = "images/mist.png"; 
+        }       
     }
 
     searchBtn.addEventListener("click", () => {
